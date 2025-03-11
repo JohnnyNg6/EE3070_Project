8282d3d0a46a36bcd71137d7bf0c1a206fb27bf1
johnny <104272459+JohnnyNg6@users.noreply.github.com>
2025-03-11 20:57:14 +0800

    Update main.ino

--- main.ino
+++ main.ino
@@ -24,7 +24,7 @@
 
   // Counting channel details
   unsigned long counterChannelNumber = 298725;
-  const char * myCounterReadAPIKey = "SODG0O2UZVGKWAWG";
+  const char * myCounterReadAPIKey = "SODG0O2UZVGKWAW";
   unsigned int counterFieldNumber = 1; 
 
   //Read_Function
@@ -40,7 +40,42 @@
   String myStatus = "";
 //}
 
+//{INA219_Voltage_Measurement
+  /*!
+  *@file getVoltageCurrentPower.ino
+  *@brief Get the current, voltage, and power of electronic devices.
+  *@copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+  *@license     The MIT license (MIT)
+  *@author [fengli](li.feng@dfrobot.com)
+  *@version  V1.0
+  *@date  2022-3-1
+  *@url https://github.com/DFRobot/DFRobot_INA219
+  */
 
+  #include <Wire.h>
+  #include "DFRobot_INA219.h"
+  #include "volt_measure.h"
+
+
+  /**
+  * @fn DFRobot_INA219_IIC
+  * @brief pWire I2C controller pointer
+  * @param i2caddr  I2C address
+  * @n INA219_I2C_ADDRESS1  0x40   A0 = 0  A1 = 0
+  * @n INA219_I2C_ADDRESS2  0x41   A0 = 1  A1 = 0
+  * @n INA219_I2C_ADDRESS3  0x44   A0 = 0  A1 = 1
+  * @n INA219_I2C_ADDRESS4  0x45   A0 = 1  A1 = 1	 
+    */
+  DFRobot_INA219_IIC     ina219(&Wire, INA219_I2C_ADDRESS4);
+
+  // Revise the following two paramters according to actual reading of the INA219 and the multimeter
+  // for linearly calibration
+  float ina219Reading_mA = 1000;
+  float extMeterReading_mA = 1000;
+
+
+
+//}
 
 void setup() {
 
@@ -72,11 +107,27 @@ void setup() {
   ThingSpeak.begin(client);  // Initialize ThingSpeak
 //}
 
+//{INA219_Volt_Measurement_SetUp
+  Serial.begin(115200);
+  //Open the serial port
+  while(!Serial);
+  
+  Serial.println();
+  //Initialize the sensor
+  while(ina219.begin() != true) {
+      Serial.println("INA219 begin faild");
+      delay(2000);
+  }
+  //Linear calibration
+  ina219.linearCalibrate(/*The measured current before calibration*/ina219Reading_mA, /*The current measured by other current testers*/extMeterReading_mA);
+  Serial.println();
+//}
 }
 
 
 
 void loop() {
   //WiFi_Read();
-  WiFi_Write();
+  //WiFi_Write();
+  Voltage_Measure();
 }
