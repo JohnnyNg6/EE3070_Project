df7f416c6ad8a6be19a07deb6d6a4c9518629c04
johnny <104272459+JohnnyNg6@users.noreply.github.com>
2025-03-11 20:58:11 +0800

    Create volt_measure.cpp

---
+++ volt_measure.cpp
@@ -0,0 +1,18 @@
+#include "volt_measure.h"
+
+void Voltage_Measure(void){
+    Serial.print("BusVoltage:   ");
+    Serial.print(ina219.getBusVoltage_V(), 2);
+    Serial.println("V");
+    Serial.print("ShuntVoltage: ");
+    Serial.print(ina219.getShuntVoltage_mV(), 3);
+    Serial.println("mV");
+    Serial.print("Current:      ");
+    Serial.print(ina219.getCurrent_mA(), 1);
+    Serial.println("mA");
+    Serial.print("Power:        ");
+    Serial.print(ina219.getPower_mW(), 1);
+    Serial.println("mW");
+    Serial.println("");
+    delay(1000);
+}
